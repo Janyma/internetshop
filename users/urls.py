703464d6e84fef3ae3643cbeab746b1b1e96b016
Zhanyl <123janyl456@gmail.com>
2024-04-10 17:58:11 +0200

    Profile

--- users/urls.py
+++ users/urls.py
@@ -4,10 +4,12 @@ from django.urls import path, include
 from main import views
 from django.views.generic.base import TemplateView
 from users.views import register
+from users.views import profile
 
 urlpatterns = [
  
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('profile/', profile, name='profile')
 
 ]
